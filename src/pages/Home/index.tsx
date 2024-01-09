0e868b794364a77cc0523c30f97bcd769a63aeec
tramtln <vuanlbv@gmail.com>
2024-01-09 21:06:33 +0700

    Install ant design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,16 @@
-export default function Home(){
-    return <div>Hello</div>
+import { Input } from "antd";
+
+export default function Home() {
+	return (
+		<div className="p-4">
+			<div className="grid grid-cols-3 gap-4 mb-4">
+				<div className="col-span-1">
+					<Input type="text" placeholder="Date" className="input input-bordered w-full" />
+				</div>
+				<div className="col-span-1">
+					<Input type="text" placeholder="Time" className="input input-bordered w-full" />
+				</div>
+			</div>
+		</div>
+	);
 }
