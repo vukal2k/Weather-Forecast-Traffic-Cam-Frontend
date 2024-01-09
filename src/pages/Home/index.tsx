33b5bf629de02823f62283bfa44ddb7fa5bd773b
tramtln <vuanlbv@gmail.com>
2024-01-09 21:18:50 +0700

    Revert "Uninstall antd"
    This reverts commit 55c1ec301b99e7579977d69aa50e00cd4076dd39.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
+import { Input } from "antd";
+
 export default function Home() {
 	return (
 		<div className="p-4">
 			<div className="grid grid-cols-3 gap-4 mb-4">
 				<div className="col-span-1">
-					<input type="text" placeholder="Date" className="input input-bordered w-full" />
+					<Input type="text" placeholder="Date" className="input input-bordered w-full" />
 				</div>
 				<div className="col-span-1">
-					<input type="text" placeholder="Time" className="input input-bordered w-full" />
+					<Input type="text" placeholder="Time" className="input input-bordered w-full" />
 				</div>
 			</div>
 		</div>
