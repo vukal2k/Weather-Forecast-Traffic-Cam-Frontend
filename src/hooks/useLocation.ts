ae26a13908bbcb2893888880639d20883487ddb1
tramtln <vuanlbv@gmail.com>
2024-01-14 03:32:30 +0700

    chore: Remove debugger

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -9,9 +9,7 @@ export function useLocation() {
 
   let url = useMemo(() => {
     let resultUrl = API.TRAFFIC_IMAGES.LOCATIONS.path;
-    debugger
     if (date && time) {
-      // const dateTime = moment(`${date} ${time}`, 'YYYY-MM-DD HH:mm:ss').tz('Asia/Singapore').format('yyyy-MM-DDThh:mm:ss+08:00');
       resultUrl += `?dateTime=${encodeURIComponent(moment(`${date} ${time}`, 'YYYY-MM-DD HH:mm:ss').toISOString())}`;
     }
     return resultUrl;
